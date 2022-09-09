380ee3b247fc03f870eebfbfbe67031d81d2d647
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-09-10 00:10:32 +0200

    Create jobs app

--- jobfinder/settings.py
+++ jobfinder/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'categories',
-    'companies'
+    'companies',
+    'jobs'
 ]
 
 MIDDLEWARE = [
---
+++ jobs/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+from companies.models import Company
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='jobs')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
