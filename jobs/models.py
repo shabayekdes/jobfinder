c7a086de08c17cb73881c2b76e6150ee3d7078a2
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-09-11 00:55:29 +0200

    Add media url to settings

--- jobs/models.py
+++ jobs/models.py
@@ -9,7 +9,7 @@ class Job(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='jobs')
+    image = models.ImageField(upload_to='media/jobs')
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
