e70c4c5e04b204bf2654d805d2e8a6d08fe08733
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-09-12 23:11:40 +0200

    Add career level attr to job model

--- jobs/models.py
+++ jobs/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-
 # Create your models here.
 from companies.models import Company
+from categories.models import Category
+
+
+class CareerLevelStatus(models.TextChoices):
+    ENTRY_LEVEL = '1', 'Entry-level'
+    INTERMEDIATE = '2', 'Intermediate'
+    MID_LEVEL = '3', 'Mid-level'
+    SENIOR = '4', 'Senior'
+    MANAGE = '5', 'Manager'
 
 
 class Job(models.Model):
@@ -11,6 +19,9 @@ class Job(models.Model):
     active = models.BooleanField(default=True)
     image = models.ImageField(upload_to='media/jobs')
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    career_level = models.CharField(max_length=1, choices=CareerLevelStatus.choices,
+                                    default=CareerLevelStatus.ENTRY_LEVEL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
