5bf4e8a760b0a921f0137fbb7d1fedf92f705508
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-09-24 20:49:30 +0200

    Add candidate profile migrations

--- candidates/models.py
+++ candidates/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-
-# Create your models here.
 
 class User(AbstractUser):
     class Role(models.TextChoices):
@@ -33,3 +33,14 @@ class Candidate(User):
 
     class Meta:
         proxy = True
+
+
+@receiver(post_save, sender=Candidate)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "CANDIDATE":
+        Profile.objects.create(user=instance)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    candidate_id = models.IntegerField(null=True, blank=True)
